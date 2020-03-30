1acda1349a52a59a1a606b587a59b5d32f7eca01
Michael Notarnicola <michael@beardphantom.com>
2020-03-29 19:25:40 -0700

    Added missing scripts test

---
+++ Tests/MissingScriptsTests.cs
@@ -0,0 +1,43 @@
+﻿using EditorEssentials.Editor;
+using System.Collections;
+using System.Linq;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    public class MissingScriptsTests
+    {
+        #region Methods
+
+        [UnityTest]
+        public IEnumerator NoMissingScripts_Prefabs()
+        {
+            using (var session = new TestSession())
+            {
+                var paths = AssetDatabase.FindAssets("t:GameObject")
+                    .Select(AssetDatabase.GUIDToAssetPath);
+                foreach (var path in paths)
+                {
+                    var obj = AssetDatabase.LoadAssetAtPath<GameObject>(path);
+                    var hierarchy = obj.GetComponentsInChildren<Transform>(true).Select(t => t.gameObject).Distinct();
+                    foreach (var gameObject in hierarchy)
+                    {
+                        var missingCount = GameObjectUtility.GetMonoBehavioursWithMissingScriptCount(gameObject);
+                        if (missingCount > 0)
+                        {
+                            session.ReportErr($"{missingCount} scripts on {gameObject.transform.GetPath()}.", obj);
+                        }
+                    }
+
+                    yield return null;
+                }
+
+                yield return Resources.UnloadUnusedAssets();
+            }
+        }
+
+        #endregion
+    }
+}
